2f0bcf650fdc232bd0d2be6522dcfae104b3baca
Matias-Stadler <koutar@gmail.com>
2024-10-30 13:57:16 +0100

    shed complete

--- js/index.js
+++ js/index.js
@@ -1,25 +1,14 @@
 function countSheep(shedForSheep) {
-    const count = shedForSheep.filter(Boolean).length;
-    let sheep = 0;
-    let wolf = 0;
-    for (let i=0;count;i++){
-        if(shedForSheep[i]==true){
-            sheep=sheep+1;
-        }
-        if(shedForSheep[i]==false){
-            wolf=wolf+1;
-        }
-    }
-
+    const sheep = shedForSheep.filter(Boolean).length;
 
     if (sheep == 0) {
-        result = "UPS!!! Wolfs eaten Sheep"
+        console.log("UPS!!! Wolfs eaten Sheep")
     }
 
     if (sheep != 0) {
-        result = "There are ${sheep} sheep in total"
+       console.log("There are", sheep, "sheep in total")
     }
 }
-shedForSheep = [true,true,true,false,true,true,true,true,true,false,true,false,true,false,false,true,true,true,true,true,false,false,true,true]
+shedForSheep = [true,true,true,false,true,true,true,true,true,false,true,false,true,false,false,true,true,true,true,true,false,false,true,true];
 
-console.log(countSheep(shedForSheep));
+countSheep(shedForSheep);
